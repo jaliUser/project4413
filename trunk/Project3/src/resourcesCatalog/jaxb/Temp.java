f108480a6d6ea10ada9cd642ddbeed0d07f6f580
raeidsaqur <raeidsaqur@9fcd4155-2364-a30a-4d9a-c5e54cf92bc8>
2010-12-05 18:21:43 +0000

    model completed

---
+++ trunk/Project3/src/resourcesCatalog/jaxb/Temp.java
@@ -0,0 +1,61 @@
+package resourcesCatalog.jaxb;
+
+import java.util.List;
+
+import javax.xml.datatype.XMLGregorianCalendar;
+
+public class Temp {
+
+}
+/*
+ public Bookings()
+    {
+    	super();
+    }
+    
+    public Bookings(int bid, String bName, String bDesc, int uid, int rid, XMLGregorianCalendar cal, boolean isItr)
+    {
+    	this.bookingID = bid;
+    	this.bookingName = bName;
+    	this.bookingDescription = bDesc;
+    	this.userID = uid;
+    	this.resourceID = rid;
+    	this.bookingDate = cal;
+    	this.isIterative = isItr;
+    }
+    
+
+
+
+ public Resources()
+    {
+    	super();
+    }
+    public Resources(int rID, String rName, String rDesc, byte [] img, List<Bookings> bks)
+    {
+    	
+    	this.resourceID = rID;
+    	this.resourceName = rName;
+    	this.description = rDesc;
+    	this.picture = img;
+    	this.bookings = bks;
+    }
+    
+     public Users()
+    {
+    	super();
+    }
+    public Users(int uid, boolean isAdmin, String ln, String fn, String pn, String add, String email)
+    {
+    	this.userID = uid;
+    	this.isAdmin = isAdmin;
+    	this.lastName = ln;
+    	this.firstName = fn;
+    	this.phone = pn;
+    	this.address = add;
+    	this.email = email;
+    	
+    }
+
+
+*/
